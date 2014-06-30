2fb56c92a7128da840420e2897ecbb39cbcbae81
Eddy Garcia <eddy@thehackerati.com>
2014-06-30 11:45:54 -0400

    added source of message

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var morgan = require('morgan');
+var os=require('os');
 
 var app = express();
 app.use(morgan('dev'));
 
+var addr = "None";
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('Local address: '+add);
+    addr = add;
+});
 
 app.get('/', function(req, res){
-      res.send('Hello World');
+      res.send('Hello World from : ' + addr);
 });
 
 app.listen(3000);
 console.log('Server listening on port: 3000');
+
