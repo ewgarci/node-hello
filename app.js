88d496dcf1498bded5e2e4ca5120d38d15489e4a
Eddy <Eddy@thehackerati.com>
2014-09-05 03:45:54 -0400

    add timestamp to message

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ var os=require('os');
 var app = express();
 app.use(morgan('dev'));
 
-var addr = "None";
-require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-    console.log('Local address: '+add);
-    addr = add;
-});
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (k in interfaces) {
+    for (k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family == 'IPv4' && !address.internal) {
+            addresses.push(address.address)
+        }
+    }
+}
 
 app.get('/', function(req, res){
-      res.send('Hello World from : ' + addr);
+        var date = new Date();
+        res.send(date + ': Hello World from ' + addresses[0]);
 });
 
 app.listen(process.env.PORT || 3000);
+console.log('Local address: '+addresses[0]);
 console.log('Server listening on port: ' + (process.env.PORT || 3000));
-
